b17170854908bb523d946f11229343579aab2f68
lara.romero <lara.romero@20201150-ASP.local>
2023-10-05 09:53:27 +0200

    update_advance structs

--- Playgrounds/Funciones.playground/Pages/Funciones avanzado.xcplaygroundpage/Contents.swift
+++ Playgrounds/Funciones.playground/Pages/Funciones avanzado.xcplaygroundpage/Contents.swift
@@ -18,6 +18,7 @@ print(montarSaludo(nombre: "María"))
 
 print("=========")
 
+//para obviar el nombre
 func montarSaludo(_ nombre: String) -> String {
     return "Hola, \(nombre)"
 }
@@ -26,6 +27,7 @@ print(montarSaludo("Laura"))
 
 print("=========")
 
+//poniéndole un nombre interno y otro externo al parámetro
 func montarSaludo(n nombre: String) -> String {
     return "Hola, \(nombre)"
 }
@@ -34,6 +36,7 @@ print(montarSaludo(n: "Ana"))
 
 print("=========")
 
+//con throws, haciendo el bloque do/catch
 func cargarDatos() throws -> Data {
     return try Data(contentsOf: URL(string: "/algo.com")!)
 }
@@ -47,40 +50,44 @@ do {
 print("=========")
 
 func devuelveNumero() -> Int {
-    Int.random(in: 0...100)
+    Int.random(in: 0...100)       //Cuando tenemos una función que solo tiene una línea podemos obviar el return
 }
 
 print(devuelveNumero())
 
 print("=========")
 
+// Las funciones están diseñadas para devolver un solo tipo de dato
+// Si puntualmente queremos que devuelva más de uno:
 func devuelveNumeros() -> (Int, Int) {
-    (Int.random(in: 0...100), Int.random(in: 0...100))
+    (Int.random(in: 0...100), Int.random(in: 0...100))            //-> tupla de elementos (omitimos return)
 }
 
 print(devuelveNumeros())
 
 print("=========")
+
+// Queremos volcar los datos de "a" en "b" y de "b" en "a"
 var a: Int = 10
 var b: Int = 20
 
 func intercambiaDatos(a: inout Int, b: inout Int) {
-    let c = a
-    a = b
+    let c = a                            // Estos valores pasan a ser constantes y como estamos en tipos por valor, no podríamos variar su contenido
+    a = b                                 // para poder hacerlo, usamos el "inout"  -> Estos datos cambian en este momento a ser "datos por referencia"                                                      -> el cambio será fuera del ámbito
     b = c
 }
 
 print("a: \(a)")
 print("b: \(b)")
-intercambiaDatos(a: &a, b: &b)
+intercambiaDatos(a: &a, b: &b)              //Al utilizar el inout, tenemos que poner los valores con & delante
 print("a: \(a)")
 print("b: \(b)")
 
 print("=========")
 
-func suma(_ numeros: Int...) -> Int {
+func suma(_ numeros: Int...) -> Int {         //Int...    -> numero variable de enteros
     var result = 0
-    for numero in numeros {
+    for numero in numeros {                // es como si fuera un array
         result += numero
     }
     return result
@@ -90,6 +97,7 @@ print(suma(2, 3, 44, 3, 1, 98))
 
 print("=========")
 
+//función dentro de función
 func saluda() {
     func cargarSaludo(nombre: String) -> String {
         "Saludos, \(nombre)"
@@ -102,6 +110,8 @@ saluda()
 
 print("=========")
 
+// Las funciones son parámetros.
+//Podemos almacenar funciones en una variable para usarlas después
 var parametroSaluda: () -> Void = saluda // Function type = Parámetros de entrada + parámetros de salida
 
 parametroSaluda()
