7e0ab9200ff46e35522e2d980a310aebb3e7db47
Lara <lara.romero@alten.es>
2023-10-10 12:35:03 +0200

    UIKit_firstUpdate

---
+++ Playgrounds/PruebaUIKit/PruebaUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaUIKit
+//
+//  Created by lara.romero on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
